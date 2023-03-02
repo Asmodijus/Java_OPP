aab89a04c20a33fcd2fb5fdecaa660c555cbb796
Asmodijus <edvinas.junevicius+github@gmail.comm>
2023-03-02 21:23:26 +0200

    comit

--- Egzam/src/main/java/lt/code/academy/Operation.java
+++ Egzam/src/main/java/lt/code/academy/Operation.java
@@ -8,7 +8,6 @@ import com.fasterxml.jackson.databind.type.MapType;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
